26bbe741b8d75347103ab156cc2ca31e22db2443
selenacdeng <selenacdeng@gmail.com>
2023-07-19 15:00:10 -0700

    New plot for 3c and updated date for 3b

--- homework 1/MAE143Ahw1_3c.m
+++ homework 1/MAE143Ahw1_3c.m
@@ -1,9 +1,9 @@
 %Selena Deng 7/19/23
 
-%Bode plot of D(s) = K2*(s+1)/(s+100) = U(s)/E(s) (hw1 q2b)
+%Bode plot of D(s)*G(s) 
 
 clear;close all;
 
-G1=tf([0 1 1],[0 1 100]); %creates the transfer function D(s)
+G1=tf([0 1 1],[1 100 0 0]); %creates the transfer function D(s)*G(s)
 plot_options=bodeoptions; plot_options.MagScale='log'; plot_options.MagUnits='abs'; %plots bode plot with log scale
 h=bodeplot(G1,plot_options);
