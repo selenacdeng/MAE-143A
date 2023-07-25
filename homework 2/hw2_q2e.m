260b088cab1663bef36cdb0c7156c1ac8593c30d
selenacdeng <selenacdeng@gmail.com>
2023-07-24 23:08:15 -0700

    question 2 and 3 plots
    included system info command

--- homework 2/hw2_q2e.m
+++ homework 2/hw2_q2e.m
@@ -1,3 +1,4 @@
 %plots the step response of G(s) of hw 2 question 2
 sys = tf([-3 -2], [1 2 2]);%Transfer function
 step(sys) %plots the step response
+stepinfo(sys) %info about system (tr, ts, Mp)
--- homework 2/hw2_q3e.m
+++ homework 2/hw2_q3e.m
@@ -1,3 +1,4 @@
-%plots the step response of G(s) of hw 2 question 2
+%plots the step response of G(s) of hw 2 question 3
 sys = tf([1], [ 1 5]);%Transfer function
 step(sys) %plots the step response
+stepinfo(sys) %info about system (tr, ts, Mp)
